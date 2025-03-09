7078e2905f800b5b0aa1c6f46deb9ebca272d891
katsugrace <victor.drobizov@gmail.com>
2025-03-09 18:24:31 +0500

    added skeleton test

---
+++ plane_builder/test/plane_builder.cpp
@@ -0,0 +1,14 @@
+// Copyright 2025 Victor Drobizov
+
+#include <gtest/gtest.h>
+
+class PlaneBuilderTest
+  : public testing::Test
+{
+
+};
+
+TEST_F(PlaneBuilderTest, MAIN)
+{
+  std::cout << "Hello i'm test" << std::endl;
+}
