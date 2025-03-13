4f4e13950351c99141a8800e37f92a9e4e1b0336
katsugrace <victor.drobizov@gmail.com>
2025-03-13 21:09:04 +0500

    added implementation of plane construction

--- plane_builder/src/plane_builder.cpp
+++ plane_builder/src/plane_builder.cpp
@@ -39,22 +39,48 @@ std::string PlaneBuilder::GetChildFrame() const noexcept
 }
 
 bool PlaneBuilder::AttachPoints(
-  const Eigen::Vector3d &,
-  const Eigen::Vector3d &,
-  const Eigen::Vector3d &)
+  const Eigen::Vector3d & first_point,
+  const Eigen::Vector3d & second_point,
+  const Eigen::Vector3d & third_point)
 {
+  Eigen::Vector3d first_matrtix = second_point - first_point;
+  Eigen::Vector3d second_matrtix = third_point - first_point;
+
+  Eigen::Vector3d normal = first_matrtix.cross(second_matrtix);
+  normal.normalize();
+
+  auto quaterniond = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitZ(), normal);
+
+  transfrom_stamped_.transform.rotation.x = quaterniond.x();
+  transfrom_stamped_.transform.rotation.y = quaterniond.y();
+  transfrom_stamped_.transform.rotation.z = quaterniond.z();
+  transfrom_stamped_.transform.rotation.w = quaterniond.w();
+
   return true;
+}
+
+bool PlaneBuilder::AttachPoints(
+  const geometry_msgs::msg::Point & first_point,
+  const geometry_msgs::msg::Point & second_point,
+  const geometry_msgs::msg::Point & third_point)
+{
+  return AttachPoints(
+    Eigen::Vector3d(first_point.x, first_point.y, first_point.z),
+    Eigen::Vector3d(second_point.x, second_point.y, second_point.z),
+    Eigen::Vector3d(third_point.x, third_point.y, third_point.z));
 }
 
 void PlaneBuilder::publishPlaneCb()
 {
-  geometry_msgs::msg::TransformStamped transfrom_stamped;
+  transfrom_stamped_.header.stamp = this->now();
+  transfrom_stamped_.header.frame_id = header_frame_;
+  transfrom_stamped_.child_frame_id = child_frame_;
 
-  transfrom_stamped.header.stamp = this->now();
-  transfrom_stamped.header.frame_id = header_frame_;
-  transfrom_stamped.child_frame_id = child_frame_;
+  transfrom_stamped_.transform.translation.x = 0.0;
+  transfrom_stamped_.transform.translation.x = 0.0;
+  transfrom_stamped_.transform.translation.x = 1.1;
 
-  broadcaster_->sendTransform(transfrom_stamped);
+  broadcaster_->sendTransform(transfrom_stamped_);
 }
 
 void PlaneBuilder::declareParameters()
